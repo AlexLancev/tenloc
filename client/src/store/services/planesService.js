7cd52e2f6e40dbdefa3a6715ca4d0974358da406
Aleksei Vavulo <aleksei.lancevv@gmail.com>
2024-07-20 16:57:40 +0500

    update app

--- client/src/store/services/planesService.js
+++ client/src/store/services/planesService.js
@@ -1,22 +1,22 @@
 import axios from "axios";
 
 const getPlanes = async () => {
-    const planes = await axios.get('/tenloc/api/tours');
+    const planes = await axios.get('/api/tours');
     return planes.data;
 }
 
 const getBlog = async () => {
-    const planes = await axios.get('/tenloc/api/blog');
+    const planes = await axios.get('/api/blog');
     return planes.data;
 }
 
 const getPlane = async (id) => {
-    const planes = await axios.get(`/tenloc/api/tours/${id}`);
+    const planes = await axios.get(`/api/tours/${id}`);
     return planes.data;
 }
 
 const getBlogArticle = async (id) => {
-    const planes = await axios.get(`/tenloc/api/blog/${id}`);
+    const planes = await axios.get(`/api/blog/${id}`);
     return planes.data;
 }
 
